1c0ed328e634de50449daaa0bffde1cb3299191b
Aagam fashion <aagamfashion@192.168.0.144>
2025-09-18 22:10:03 +0530

    Add blog in admin file

--- cms/admin.py
+++ cms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from cms.models.faq import FAQ
 
-from .models.blog import ContactMessage
+from .models.blog import BlogPost, ContactMessage
 from .models.product import ProductCategory
 
 admin.site.register(ProductCategory)
@@ -16,3 +16,8 @@ class FAQAdmin(admin.ModelAdmin):
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "email")
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "category", "slug", "status", "views_count", "reading_time","tags","content","featured_image","meta_title","meta_description")
+     
