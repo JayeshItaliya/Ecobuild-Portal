ca0ad865a698ba57b8571a6ac1da8c796688e3f3
Aagam fashion <aagamfashion@192.168.0.144>
2025-09-18 22:16:28 +0530

    Added tag and category in admin

--- cms/admin.py
+++ cms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from cms.models.faq import FAQ
 
-from .models.blog import BlogPost, ContactMessage
+from .models.blog import BlogPost, Category, ContactMessage, Tag
 from .models.product import ProductCategory
 
 admin.site.register(ProductCategory)
@@ -21,3 +21,10 @@ class ContactMessageAdmin(admin.ModelAdmin):
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "category", "slug", "status", "views_count", "reading_time","tags","content","featured_image","meta_title","meta_description")
      
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
